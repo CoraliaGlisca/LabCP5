b462d4e96246f63c09a1884f1dcf799a3dfbe44c
CoraliaGlisca <32877341+CoraliaGlisca@users.noreply.github.com>
2017-11-15 09:47:16 +0200

    Update main.c

--- II2/main.c
+++ II2/main.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-#include<math.h>
+#include<stdlib.h>
 
+int n,j,k,i;
+float a[100],t;
 int main()
 {
-    float a[100], b[100];
-    int n,i;
-    int k=0;
-
-    printf("Enter the number of elements:");
-    scanf("%d", &n);
+     printf("Enter the number of elements:");
+     scanf("%d", &n);
 
     for(i=0;i<n;i++)
+        {
+            printf("\nElements of a[%d]:", i+1);
+            scanf("%f", &a[i]);
+        }
+    for(j=1;j<n;j+=2)
     {
-        printf("\nElements of a[%d]:", i+1);
-        scanf("%d", &a[i]);
+        t=a[j];
+        a[j]=(t+a[j-1])/2;
+        n++;
+        for(k=n-1;k>j+1;k--)
+        {
+            a[k]=a[k-1];
+        }
+        a[j+1]=t;
     }
-
-    for(i=0;i<=n;i++)
-    {
-        b[k]=a[i];
-        b[k+1]=(a[i]+a[i+1])/2;
-        k+=2;
-    }
-
-    for(i=0;i<=k-2;i++)
-        printf("%2.1f ", a[i]);
+    for(i=0;i<n;i++)
+         printf("%2.1f ", a[i]);
+    return 0;
 }
